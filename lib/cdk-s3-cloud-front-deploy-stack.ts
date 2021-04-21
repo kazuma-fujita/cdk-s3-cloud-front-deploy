990ca2707c0db938b87c18cbfce7222cbef95e82
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-04-21 11:46:51 +0900

    first commit

--- lib/cdk-s3-cloud-front-deploy-stack.ts
+++ lib/cdk-s3-cloud-front-deploy-stack.ts
@@ -1,9 +1,19 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkS3CloudFrontDeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "sample-bucket";
+    const originBucket = new s3.Bucket(this, "S3Bucket", {
+      bucketName: bucketName,
+      // Bucketへの直接アクセスを禁止
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      // CDK Stack削除時にBucketも削除する
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
